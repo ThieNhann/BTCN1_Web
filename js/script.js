756daa9a68d4559bf43d2e88bad1f3b13f6a6b69
23120064 <ngtnhan14@gmail.com>
2025-10-19 19:08:33 +0700

    news: drag n drop feature

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function()  {
-    console.log("loaded");
-    const sidebar = document.querySelector('.aside');
+    const sidebar = document.querySelector('aside');
     const newsItems = document.querySelectorAll('.news-item');
 
     newsItems.forEach(item => {
@@ -9,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function()  {
         let isActive = item.classList.contains('active');
         header.addEventListener('click', () => {
             item.classList.toggle('active');
-            isActive = !isActive;
+            isActive = item.classList.contains('active');
             if (isActive) { 
                 icon.textContent = '|';
             }
@@ -17,21 +16,47 @@ document.addEventListener('DOMContentLoaded', function()  {
                 icon.textContent = '▶';
                 icon.style.fontSize = '20px';
             }
-        })  
-    })
+        });
 
-    let draggedItem = null;
-
-    newsItems.forEach(item => {
-        item.addEventListener('dragstart', () => {
+        header.addEventListener('dragstart', () => {
+            console.log("dragged")
             draggedItem = item;
             setTimeout(() => {
                 item.classList.add('dragging');
-            });
+            }, 0);
         });
 
         item.addEventListener('dragend', () => {
             item.classList.remove('dragging')
         })
-    });
+    })
+
+    let draggedItem = null;
+
+    sidebar.addEventListener('dragover', e => {
+        e.preventDefault();
+        const afterElement = getDragAfterElement(sidebar, e.clientY);
+
+        if (afterElement == null) {
+            sidebar.appendChild(draggedItem);
+        } else {
+            sidebar.insertBefore(draggedItem, afterElement);
+        }
+
+    }) 
+
+    function getDragAfterElement(container, y) {
+        const draggableElements = [...container.querySelectorAll('.news-item:not(.dragging)')];
+
+        return draggableElements.reduce((closest, child) => {
+            const box = child.getBoundingClientRect();
+            const offset = y - box.top - box.height / 2;
+
+            if (offset < 0 && offset > closest.offset) {
+                return { offset: offset, element: child };
+            } else {
+                return closest;
+            }
+        }, {offset: Number.NEGATIVE_INFINITY }).element;
+    }
 })
