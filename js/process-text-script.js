6257ce29c415197864ccc9fef63fbcd34776e84d
23120064 <ngtnhan14@gmail.com>
2025-10-20 17:09:04 +0700

    process-text: button queries, highlight event listener

--- js/process-text-script.js
+++ js/process-text-script.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     let sample = document.querySelector('.sampleStyle span');
+    sample.style.color = '#ff0000';
+    sample.style.backgroundColor = '#ffff00';
+    sample.style.fontWeight = 'normal';
+    sample.style.fontStyle = 'normal';
+    sample.style.textDecoration = 'none';
 
+    const originalParagraph = document.querySelector('.text-field p');
+    const paragraph = document.querySelector('.text-field p');
+    const originalTextContent = originalParagraph.textContent;
     const textColorPicker = document.getElementById('textColor');
     const backgroundColorPicker = document.getElementById('backgroundColor');
     const boldCheckbox = document.getElementById('boldCheckbox');
@@ -8,6 +16,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const underlineCheckbox = document.getElementById('underlineCheckbox');
     const settingButton = document.querySelector('.moreSettingButton');
     const popupBox = document.getElementById('settingsPopup');
+    popupBox.style.display = 'none';
+    const highlightButton = document.getElementById('highlightButton');
+    const deleteButton = document.getElementById('deleteButton');
+    const resetButton = document.getElementById('resetButton');
+
 
     textColorPicker.addEventListener('input', (event) => {
         sample.style.color = event.target.value;
@@ -53,4 +66,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    highlightButton.addEventListener('click', () => {
+        const textToHighlight = document.getElementById('sampleTextField').value;
+        highlight(textToHighlight);
+    });
 });
